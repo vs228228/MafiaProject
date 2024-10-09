6b8313ad785d58925748a7db22c399163a44a2ef
IVAN <ivan_net04@mail.ru>
2024-10-09 20:52:07 +0300

    Add Vote and Lobby services. Needed to create DTO

---
+++ MafiaProject.Server/Application/interfaces/ILobbyService.cs
@@ -0,0 +1,14 @@
+﻿using MafiaProject.Server.Application.DTO;
+
+namespace MafiaProject.Server.Application.interfaces
+{
+    public  interface ILobbyService
+    {
+        //public Task<LobbyDTO> GetLobbyByIdAsync(int id);
+        //public Task<IEnumerable<LobbyDTO>> GetAllLobbiesAsync();
+        //public Task CreateLobbyAsync(LobbyCreateDTO lobbyCreateDTO);
+        //public Task UpdateLobbyAsync(LobbyUpdateDto lobbyUpdateDTO);
+        public Task DeleteLobbyAsync(int id);
+        public Task StartGameAsync(int lobbyId);
+    }
+}
