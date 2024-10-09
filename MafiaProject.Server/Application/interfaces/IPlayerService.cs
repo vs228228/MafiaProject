4f1b49d4f21447e4d6498466a54526c0d03e52a8
Ivan Starovoytov <vs228228@gmail.com>
2024-10-09 22:08:20 +0300

    sone changes at core layer

---
+++ MafiaProject.Server/Application/interfaces/IPlayerService.cs
@@ -0,0 +1,19 @@
+﻿using MafiaProject.Server.Core.Entityes;
+
+namespace MafiaProject.Server.Application.interfaces
+{
+    public interface IPlayerService
+    {
+       /* Task<PlayerDTO> GetPlayerByUserIdAsync(int userId);
+        Task<IEnumerable<PlayerDTO>> GetAllPlayersAsync(int gameId);
+        Task<PlayerDTO> ConvertUserToPlayerAsync(User user, int connectionId); // в теории может быть нужно убрать connectionId
+        Task UpdatePlayerAsync(PlayerUpdateDTO playerUpdateDTO);
+        Task RemovePlayerAsync(int playerId);
+        Task ChangeReadyAsync(ChangeReadyDTO);
+        Task ChangeMicroAsync(ChangeMicroDTO);
+        Task ChangeVideoAsync(ChangeVideoDTO);
+       // Task ChangeLiveAsync(ChangeLiveDTO);  // пересмотреть необходимость в этом методе
+
+        */
+    }
+}
