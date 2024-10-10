bdf93f9a5f294452e6f1eca2b1f153d7242c7a41
IVAN <ivan_net04@mail.ru>
2024-10-10 19:10:23 +0300

    Add two services and their dto

--- MafiaProject.Server/Application/interfaces/ILobbyService.cs
+++ MafiaProject.Server/Application/interfaces/ILobbyService.cs
@@ -4,10 +4,10 @@ namespace MafiaProject.Server.Application.interfaces
 {
     public  interface ILobbyService
     {
-        //public Task<LobbyDTO> GetLobbyByIdAsync(int id);
-        //public Task<IEnumerable<LobbyDTO>> GetAllLobbiesAsync();
-        //public Task CreateLobbyAsync(LobbyCreateDTO lobbyCreateDTO);
-        //public Task UpdateLobbyAsync(LobbyUpdateDto lobbyUpdateDTO);
+        public Task<LobbyDTO> GetLobbyByIdAsync(int id);
+        public Task<IEnumerable<LobbyDTO>> GetAllLobbiesAsync();
+        public Task CreateLobbyAsync(LobbyCreateDTO lobbyCreateDTO);
+        public Task UpdateLobbyAsync(LobbyUpdateDTO lobbyUpdateDTO);
         public Task DeleteLobbyAsync(int id);
         public Task StartGameAsync(int lobbyId);
     }
