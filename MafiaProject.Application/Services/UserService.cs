7f0cc1b817a001e9e134fb735472c36b764f38eb
IVAN <ivan_net04@mail.ru>
2024-10-15 15:22:46 +0300

    com

---
+++ MafiaProject.Application/Services/UserService.cs
@@ -0,0 +1,29 @@
+﻿using MafiaProject.Application.DTO;
+using MafiaProject.Application.interfaces;
+using MafiaProject.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MafiaProject.Application.Services
+{
+    //public class UserService : IUserService
+    //{
+    //    private readonly IUnitOfWork _unitOfWork;
+
+    //    public UserService(IUnitOfWork unitOfWork)
+    //    {
+    //        _unitOfWork = unitOfWork;
+    //    }
+
+    //    public async Task<UserDTO> GetUserByEmailAsync(string email)
+    //    {
+
+    //        var ans = await _unitOfWork.Users.GetUserByEmailAsync(email);
+
+            
+    //    }
+    //}
+}
