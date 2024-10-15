2c04c62bd9e1be706c5438dc277f421c346e5b34
IVAN <ivan_net04@mail.ru>
2024-10-15 12:24:37 +0300

    add dto and they was checked by teamleader

---
+++ MafiaProject.Server/Application/DTO/CreateGameDTO.cs
@@ -0,0 +1,10 @@
+﻿using MafiaProject.Server.Core.Entityes;
+
+namespace MafiaProject.Server.Application.DTO
+{
+    public class CreateGameDTO
+    {
+        public string Name { get; set; }
+        public ICollection<Player> Players { get; set; }
+    }
+}
