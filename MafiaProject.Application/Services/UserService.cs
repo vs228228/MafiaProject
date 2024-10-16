3a1b3b7a124bb919519b35770922fa062a5d43b5
IVAN <ivan_net04@mail.ru>
2024-10-16 15:18:13 +0300

    Add mapper

--- MafiaProject.Application/Services/UserService.cs
+++ MafiaProject.Application/Services/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using MafiaProject.Application.DTO;
 using MafiaProject.Application.interfaces;
+using MafiaProject.Core.Entityes;
 using MafiaProject.Core.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -12,18 +13,20 @@ namespace MafiaProject.Application.Services
     //public class UserService : IUserService
     //{
     //    private readonly IUnitOfWork _unitOfWork;
+    //    private readonly IMapper _mapper;
 
-    //    public UserService(IUnitOfWork unitOfWork)
+    //    public UserService(IUnitOfWork unitOfWork, IMapper mapper)
     //    {
     //        _unitOfWork = unitOfWork;
+    //        _mapper = mapper;
     //    }
 
     //    public async Task<UserDTO> GetUserByEmailAsync(string email)
     //    {
 
     //        var ans = await _unitOfWork.Users.GetUserByEmailAsync(email);
+    //        return await _mapper.Map<User, UserDTO>(ans);
 
-            
     //    }
     //}
 }
---
+++ MafiaProject.Application/interfaces/IMapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MafiaProject.Application.interfaces
+{
+    public interface IMapper
+    {
+        Task<TDestination> Map<TSource, TDestination>(TSource source);
+        Task<TDestination> Update<TSource, TDestination>(TSource source, TDestination destination);
+    }
+}
