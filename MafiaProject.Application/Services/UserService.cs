c9ee31d0268c2daf7c1aa910abac2df49092f289
IVAN <ivan_net04@mail.ru>
2024-10-20 10:15:07 +0300

    Add some services

--- MafiaProject.Application/Services/UserService.cs
+++ MafiaProject.Application/Services/UserService.cs
@@ -1,7 +1,9 @@
 ﻿using MafiaProject.Application.DTO;
 using MafiaProject.Application.interfaces;
+using MafiaProject.Application.Pagination;
 using MafiaProject.Core.Entityes;
 using MafiaProject.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,23 +12,66 @@ using System.Threading.Tasks;
 
 namespace MafiaProject.Application.Services
 {
-    //public class UserService : IUserService
-    //{
-    //    private readonly IUnitOfWork _unitOfWork;
-    //    private readonly IMapper _mapper;
+    public class UserService : IUserService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapperClass _mapper;
 
-    //    public UserService(IUnitOfWork unitOfWork, IMapper mapper)
-    //    {
-    //        _unitOfWork = unitOfWork;
-    //        _mapper = mapper;
-    //    }
+        public UserService(IUnitOfWork unitOfWork, IMapperClass mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
 
-    //    public async Task<UserDTO> GetUserByEmailAsync(string email)
-    //    {
+        public Task DeleteUserAsync(int id)
+        {
+            _unitOfWork.Users.DeleteAsync(id);
+            throw new NotImplementedException(); // not ready
+        }
 
-    //        var ans = await _unitOfWork.Users.GetUserByEmailAsync(email);
-    //        return await _mapper.Map<User, UserDTO>(ans);
+        public async Task<IEnumerable<UserDTO>> GetAllUsersAsync()
+        {
+            var ans = await _unitOfWork.Users.GetAllAsync();
+            return await _mapper.Map<IEnumerable<User>, IEnumerable<UserDTO>>(ans);
+        }
 
-    //    }
-    //}
+        public Task<PagedResult<UserDTO>> GetPaginatedUsersAsync(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserDTO> GetUserByEmailAsync(string email)
+        {
+
+            var ans = await _unitOfWork.Users.GetUserByEmailAsync(email);
+            return await _mapper.Map<User, UserDTO>(ans);
+
+        }
+
+        public async Task<UserDTO> GetUserByIdAsync(int id)
+        {
+            var ans = await _unitOfWork.Users.GetByIdAsync(id);
+            return await _mapper.Map<User, UserDTO>(ans);
+        }
+
+        public Task<string> RefreshTokenAsync(RefreshTokenDTO refreshTokenDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TryAddUserAsync(UserCreateDTO userCreateDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TokenDTO> TryAuthUserAsync(AuthDTO authDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateUserAsync(UserUpdateDto userUpdateDTO, IFormFile photo)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
