a26b95d4ced9e4fd13aa872a65c3afe7e7211fb3
YuliaBondar <uliabondar359@gmail.com>
2024-10-29 12:05:51 +0300

    add input changes

--- mafiaproject.client/src/shared/ModalWindow/ModalWindow.jsx
+++ mafiaproject.client/src/shared/ModalWindow/ModalWindow.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './ModalWindow.css';
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import Button from '../Button/Button'
 import Input from '../Input/Input'
+import { IoEyeSharp } from "react-icons/io5";
+import { FaEyeSlash } from "react-icons/fa6";
+
 
 const ModalWindow = ({ isOpen, onClose, modalType }) => {
+    const [showPassword, setShowPassword] = useState(false);
     if (!isOpen) return null; 
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(prev => !prev);
+    };
 
     return (
         <div className='modalWin_'>
@@ -17,14 +25,37 @@ const ModalWindow = ({ isOpen, onClose, modalType }) => {
                 {modalType === 'entrance' && (
                 <>
                     <Input type='text' name='roomId' label='ID комнаты' required={true} />
-                    <Input type='text' name='username' label='Имя' required={true} />
-                    <Input type='password' name='password' label='Пароль' required={false} />
+                   
+                   {/* <div className="input_with_button"> */}
+                        <Input type={showPassword ? 'text' : 'password'}  name='password' label='Пароль' required={false} />
+                        {/* <button 
+                            type="button" 
+                            className="toggle-password" 
+                            onClick={togglePasswordVisibility}
+                        >
+                            {showPassword ?  <IoEyeSharp />: <FaEyeSlash />}
+                        </button>
+                    </div>  */}
+                    {/* <button 
+                            type="button" 
+                            className="toggle-password" 
+                            onClick={togglePasswordVisibility}
+                        >
+                            {showPassword ?  <IoEyeSharp />: <FaEyeSlash />}
+                        </button> */}
                 </>
                 )}
                 {modalType === 'create' && (
                     <>
                     <Input type='text' name='lobbyName' label='Название комнаты' required={true} />
-                    <Input type='password' name='password' label='Пароль' />
+                    <Input  type={showPassword ? 'text' : 'password'} name='password' label='Пароль' />
+                    {/* <button 
+                            type="button" 
+                            className="toggle-password" 
+                            onClick={togglePasswordVisibility}
+                        >
+                            {showPassword ? <IoEyeSharp />: <FaEyeSlash />}
+                        </button> */}
                 </>
                 )}
                 <div className="button_in_lobby">
