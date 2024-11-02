0bab305af40ceefcce7f23f7195ad82e0ddb6079
YuliaBondar <uliabondar359@gmail.com>
2024-11-03 02:23:25 +0300

    add form for password recovery for

--- mafiaproject.client/src/pages/SignIn/SignIn.jsx
+++ mafiaproject.client/src/pages/SignIn/SignIn.jsx
@@ -4,29 +4,33 @@ import Button from '../../shared/Button/Button';
 import { FaArrowLeftLong } from "react-icons/fa6";
 import CheckBox from '../../shared/CheckBoxGroup/CheckBox';
 import Input from '../../shared/Input/Input';
+import { CiLock } from "react-icons/ci";
 
-const SignIn = ({showToggleButton }) => {
+const SignIn = () => {
     const [isRegistering, setIsRegistering] = useState(false);
     const [siteRulesAccepted, setSiteRulesAccepted] = useState(false);
     const [privacyPolicyAccepted, setPrivacyPolicyAccepted] = useState(false);
-
     const [showPassword, setShowPassword] = useState(false);
+    const [forgetPasswordMode, setForgetPasswordMode] = useState(false);
 
-    const togglePasswordVisibility = () => {
+
+    const togglePasswordVisibility = () => {//видимость пароля в полях ввода
         setShowPassword(prev => !prev);
     };
     
-    const handleRegisterClick = () => {
+    const handleRegisterClick = () => {//изменяет состояние приложения, чтобы отобразить форму регистрации
         setIsRegistering(true);
+        setForgetPasswordMode(false);
     };
 
-    const handleLoginClick = () => {
+    const handleLoginClick = () => {//изменяет состояние приложения, чтобы отобразить форму входа
         setIsRegistering(false);
         setSiteRulesAccepted(false);
         setPrivacyPolicyAccepted(false);
+        setForgetPasswordMode(false);
     };
 
-    const handleSiteRulesChange = () => {
+    const handleSiteRulesChange = () => {//управляет состоянием согласия пользователя с правилами сайта
         setSiteRulesAccepted(!siteRulesAccepted);
     };
 
@@ -34,56 +38,92 @@ const SignIn = ({showToggleButton }) => {
         setPrivacyPolicyAccepted(!privacyPolicyAccepted);
     };
 
+    const handleForgotPasswordClick = () => {
+        setForgetPasswordMode(true);
+    };
+
     const isRegisterButtonDisabled = !(siteRulesAccepted && privacyPolicyAccepted);
 
     return (
         <div className='signIn_Block'>
-            <h2>{isRegistering ? 'Регистрация' : 'Войти'}</h2>
-            <form>
-                <Input type='text' name='username' label='Имя пользователя' required={true}/>
-                <Input 
-                    type={showPassword ? 'text' : 'password'} 
-                    name='password' 
-                    label='Пароль' 
-                    showToggleButton 
-                    togglePasswordVisibility={togglePasswordVisibility}
-                    isPasswordVisible={showPassword}
-                />
-               
-                {isRegistering && (
-                    <>
-                    <Input type='password' name='confirm-password' label='Подтвердите пароль' required={true} />
-                    <Input type='text' name='email' label='Электронная почта' required={true} />
-                    
-                        <CheckBox
-                              siteRulesAccepted={siteRulesAccepted} 
-                              privacyPolicyAccepted={privacyPolicyAccepted}
-                              handleSiteRulesChange={handleSiteRulesChange}
-                              handlePrivacyPolicyChange={handlePrivacyPolicyChange}
+            {forgetPasswordMode ?(
+                <>
+                    <div className='Lock_icon'><CiLock size={50}/></div>
+                    <h2>Восстановление пароля</h2>
+                    <form>
+                        <Input
+                             type='email' 
+                             name='email' 
+                             label='Введите вашу электронную почту' 
+                             required={true}
                         />
-                    </>
-                )}
-            </form>
-            <div className="button_button">
-                <Button
-                        type='submit'
-                        disabled={isRegistering && isRegisterButtonDisabled}
-                        text={isRegistering ? 'Регистрация' : 'Войти'}
+                        
+                        <Button type='submit' text='Получить пароль' />
+                    </form>
+                    <FaArrowLeftLong
+                        onClick={handleLoginClick}
+                        className="login-button"
                     />
-                {!isRegistering ? (
-                    <Button
-                        onClick={handleRegisterClick}
-                        className="register-button"
-                        text='Зарегистрироваться'
-                    />
-            
+                </>
             ) : (
-                <FaArrowLeftLong
-                    onClick={handleLoginClick}
-                    className="login-button"
-                />
+                <>
+                    <h2>{isRegistering ? 'Регистрация' : 'Войти'}</h2>
+                    <form onSubmit={(e) => e.preventDefault()}>
+                        <Input type='text' name='username' label='Имя пользователя' required={true}/>
+                        <Input 
+                            type={showPassword ? 'text' : 'password'} 
+                            name='password' 
+                            label='Пароль' 
+                            showToggleButton 
+                            togglePasswordVisibility={togglePasswordVisibility}
+                            isPasswordVisible={showPassword}
+                        />
+                    
+                        {isRegistering && (
+                            <>
+                                <Input type='password' name='confirm-password' label='Подтвердите пароль' required={true} />
+                                <Input type='text' name='email' label='Электронная почта' required={true} />
+                                <CheckBox
+                                    siteRulesAccepted={siteRulesAccepted} 
+                                    privacyPolicyAccepted={privacyPolicyAccepted}
+                                    handleSiteRulesChange={handleSiteRulesChange}
+                                    handlePrivacyPolicyChange={handlePrivacyPolicyChange}
+                                />
+                            </>
+                        )}
+                    
+                    </form>
+
+                    {!isRegistering && (
+                        <span 
+                            className="forgetPassword" 
+                            onClick={handleForgotPasswordClick} 
+                        >Забыли пароль?
+                        </span>
+                    )}
+                    {!forgetPasswordMode && (
+                        <div className="button_button">
+                            <Button
+                                    type='submit'
+                                    disabled={isRegistering && isRegisterButtonDisabled}
+                                    text={isRegistering ? 'Регистрация' : 'Войти'}
+                                />
+                            {!isRegistering ? (
+                                <Button
+                                    onClick={handleRegisterClick}
+                                    className="register-button"
+                                    text='Зарегистрироваться'
+                                />
+                        ) : (
+                            <FaArrowLeftLong
+                                onClick={handleLoginClick}
+                                className="login-button"
+                            />
+                        )}
+                        </div>
+                    )}
+                </>
             )}
-            </div>
         </div>
     );
 };
