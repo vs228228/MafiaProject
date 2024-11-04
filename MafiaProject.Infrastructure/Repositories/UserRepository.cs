9ebb2ab5530cb42af20120476a50a8f5349f7efe
DenisPA <denispokhodnia@gmail.com>
2024-11-05 02:18:28 +0300

    add  User Repository and Repository
    also RefreshTokenConfiguration

---
+++ MafiaProject.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,60 @@
+﻿using MafiaProject.Core.Entityes;
+using MafiaProject.Core.Interfaces;
+using MafiaProject.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MafiaProject.Infrastructure.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public UserRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(User entity)
+        {
+            await _context.Users.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.Users
+                .OrderBy(c => c.Id)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<User?> GetByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task UpdateAsync(User entity)
+        {
+            _context.Users.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
