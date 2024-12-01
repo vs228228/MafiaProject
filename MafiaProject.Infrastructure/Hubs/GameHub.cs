df7c5d68940716d8e863e5c2c5d4a49ad8be0121
IVAN <ivan_net04@mail.ru>
2024-12-01 20:47:47 +0300

    add some fixes for game and lobby service

--- MafiaProject.Infrastructure/Hubs/GameHub.cs
+++ MafiaProject.Infrastructure/Hubs/GameHub.cs
@@ -1,8 +1,10 @@
 ﻿using MafiaProject.Application.interfaces;
+using MafiaProject.Core.Interfaces;
 using Microsoft.AspNet.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,10 +13,12 @@ namespace MafiaProject.Infrastructure.Hubs
     public class GameHub : Hub
     {
         private readonly IGameService _gameService;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public GameHub(IGameService gameService)
+        public GameHub(IGameService gameService, IUnitOfWork unitOfWork)
         {
             _gameService = gameService;
+            _unitOfWork = unitOfWork;
         }
 
         public async Task StartGame(int lobbyId)
@@ -22,6 +26,37 @@ namespace MafiaProject.Infrastructure.Hubs
             await _gameService.StartGame(lobbyId);
             await Clients.Group($"Lobby_{lobbyId}").SendAsync("StartGame", lobbyId);
         }
+
+        //public async Task OnConnectedAsync()
+        //{
+        //    // Получаем HTTP контекст из текущего подключения SignalR
+        //    var context = Context.GetHttpContext;
+
+        //    // Ищем идентификатор пользователя в клеймах (claims) текущего пользователя
+        //    // ClaimTypes.NameIdentifier обычно содержит ID пользователя
+        //    var userId = context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        //    // Пытаемся преобразовать строковый ID в число
+        //    if (int.TryParse(userId, out int parsedUserId))
+        //    {
+        //        // Если преобразование успешно, получаем пользователя из базы данных
+        //        var user = await _unitOfWork.Users.GetByIdAsync(parsedUserId);
+
+        //        // Если пользователь существует и у него есть связанный Player
+        //        if (user?.Player != null)
+        //        {
+        //            // Сохраняем ConnectionId (уникальный идентификатор SignalR подключения)
+        //            // в поле ConnectionId объекта Player
+        //            user.Player.ConnectionId = Context.ConnectionId;
+
+        //            // Сохраняем изменения в базе данных
+        //            await _unitOfWork.SaveChangesAsync();
+        //        }
+        //    }
+
+        //    // Вызываем базовую реализацию метода
+        //    await base.OnConnectedAsync();
+        //}
     }
 
 }
---
+++ MafiaProject.Infrastructure/Mapper/GameMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MafiaProject.Application.DTO;
+using MafiaProject.Core.Entityes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MafiaProject.Infrastructure.Mapper
+{
+    public class GameMappingProfile : Profile
+    {
+        public GameMappingProfile() {
+            CreateMap<CreateGameDTO, Game>();
+            CreateMap<Game, GameDTO>();
+            CreateMap<MafiaDTO, Vote>();
+        }
+    }
+}
